b436f37a0274185308449a621cd1eb4b2c33b98c
Vinícius Oliveira <vmo.estudo@gmail.com>
2021-11-25 02:29:26 -0300

    adicionando novo programa de Contas + testes

--- Primeira aplicação console/Program.cs
+++ Primeira aplicação console/Program.cs
@@ -9,6 +9,9 @@ namespace Primeira_aplicação_console
             var a = "Hello ";
             var b = "World!";
             Console.WriteLine(a + b);
+               
         }
+
     }
+
 }
---
+++ contas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Contas
+{
+	public int Soma(int a, int b)
+	{
+		return a + b;
+	}
+
+	public int Subtracao(int a, int b)
+	{
+		return a - b;
+	}
+
+	public int Multiplicacao(int a, int b)
+	{
+		return a * b;
+	}
+
+	public int Divisao(int a, int b)
+	{
+		return a / b;
+	}
+}
