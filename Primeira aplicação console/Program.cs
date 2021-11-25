d4373562fc16f95687785f8cf59aab4bede0831a
Vinícius Oliveira <vmo.estudo@gmail.com>
2021-11-25 01:07:02 -0300

    Adicionar arquivos de projeto.

---
+++ Primeira aplicação console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Primeira_aplicação_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Hello ";
+            var b = "World!";
+            Console.WriteLine(a + b);
+        }
+    }
+}
